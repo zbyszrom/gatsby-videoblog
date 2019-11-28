1ec04e3be1985d40e46d225e8f1e5260e9d861e1
Zbigniew Downar <zbyszrom@Zbigniews-iMac.local>
2019-11-28 21:01:16 +0100

    2 form test

--- gatsby-config.js
+++ gatsby-config.js
@@ -54,7 +54,8 @@ module.exports = {
         muted: false,
         autoplay: false
       }
-    },
+    }
+    /*,
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
@@ -66,7 +67,7 @@ module.exports = {
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
-    },
+    },*/
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,

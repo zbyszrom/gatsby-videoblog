4fa47dbff048ad9d0bead6b7e00808c9457aeb93
Zbigniew Downar <zbyszrom@Zbigniews-iMac.local>
2019-12-11 22:24:57 +0100

    misspelling footer

--- src/components/footer_bare.js
+++ src/components/footer_bare.js
@@ -13,7 +13,7 @@ const Footer_b = ({children}) => {
         <footer >
             {children}
             
-            <p>Created by zbyszom, © 2019</p>
+            <p>Created by zbyszrom, © 2019</p>
            <Social> <a href="https://www.facebook.com/profile.php?id=100004800559124" target="_blank" rel="noopener noreferrer"><i className="fab fa-facebook fa-2x "></i></a>
         
         <a href="https://www.youtube.com/user/zbyszrom/videos?view_as=subscriber" target="_blank" rel="noopener noreferrer"><i className="fab fa-youtube fa-2x "></i></a> 
